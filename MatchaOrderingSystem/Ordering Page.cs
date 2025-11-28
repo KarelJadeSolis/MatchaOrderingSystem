94d6139ad0a7128cee56ab2ac24bbfb69fc4b026
DELL <DELL@DESKTOP-LV1PR1Q>
2025-11-28 21:21:25 +0800

    Add project files.

---
+++ MatchaOrderingSystem/Ordering Page.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchaOrderingSystem
+{
+    public partial class Ordering_Page : Form
+    {
+        public Ordering_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
