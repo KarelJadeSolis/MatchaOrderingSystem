6d5ea8c547920fa639ea20baec19f7ac8e742b66
HeillaGraceLabitoria <labitoriaHeilla1@gmail.com>
2025-11-30 14:44:20 +0800

    add menu button

--- MatchaOrderingSystem/Landing Page.Designer.cs
+++ MatchaOrderingSystem/Landing Page.Designer.cs
@@ -28,25 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnMenu = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnMenu
             // 
-            this.button1.Location = new System.Drawing.Point(183, 312);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnMenu.Location = new System.Drawing.Point(137, 254);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(56, 19);
+            this.btnMenu.TabIndex = 0;
+            this.btnMenu.Text = "MENU";
+            this.btnMenu.UseVisualStyleBackColor = true;
             // 
             // Landing_Page
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGreen;
-            this.ClientSize = new System.Drawing.Size(800, 806);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(600, 655);
+            this.Controls.Add(this.btnMenu);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Landing_Page";
             this.Text = "Landing_Page";
             this.ResumeLayout(false);
@@ -55,6 +57,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnMenu;
     }
 }
